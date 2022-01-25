2189383ecaf0c68463a15e7b410585dadee9433c
James Etchells <jaw.etch@gmail.com>
2022-01-25 10:34:40 -0800

    Add colorChange function

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+let element = document.getElementsByClassName('panel');
 
-
-
-
-
-for (i = 0; i < 4; i++) {
+for (i = 0; i < 16; i++) {
     let div = document.createElement("div");
+
     div.className = "panel";
+    div.id = 'panel';
+    div.addEventListener('mouseover', changeColor)
     document.getElementById('container').appendChild(div);
 }
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
