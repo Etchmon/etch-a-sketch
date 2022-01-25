0e268a840ca9f7ea3b985626bf6fd613c922e777
James Etchells <jaw.etch@gmail.com>
2022-01-25 11:44:55 -0800

    Add slider display, script to capture size values

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-let element = document.getElementsByClassName('panel');
+let slider = document.getElementById('grid');
+let size = document.getElementById('size');
+
+console.log(size.innerHTML);
+
+slider.onmouseover = (e) => udpdateSize(e.target.value);
+slider.onchange = (e) => sizeChange(e.target.value);
 
 for (i = 0; i < 16; i++) {
     let div = document.createElement("div");
 
     div.className = "panel";
     div.id = 'panel';
-    div.addEventListener('mouseover', changeColor)
+    div.addEventListener('mouseover', colorChange)
     document.getElementById('container').appendChild(div);
 }
 
-function changeColor(e) {
+function udpdateSize(value) {
+    size.innerHTML = value + ' x ' + value;
+}
+
+function sizeChange(value) {
+    udpdateSize(value);
+}
+
+function colorChange(e) {
     e.target.style.backgroundColor = "black";
 }
+
+console.log(document.getElementById('grid').value)
 
